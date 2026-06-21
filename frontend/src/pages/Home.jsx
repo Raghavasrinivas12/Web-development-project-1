a1936ad79b7cd353cf56de94e174171b9803e6ca
Harish G B <harishgb3805@gmail.com>
2026-06-21 13:59:52 +0530

    added a login button in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,8 @@
 export default function Home(){
     return (
         <>
-            <h6>Home Page</h6>
+            <h1>Home Page</h1>
+            <button type='button' className="bg-blue-500 text-white shadow-lg shadow-black/30 flex-1 py-2.5 px-4 rounded-lg text-sm font-medium transition-all duration-200 cursor-pointer" onClick={()=>{location.href='/signin'}} >Login</button>
         </>
     )
 }
